a2bb39e68b952c25fc06cb2e10d4e9761ba6212f
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-09-09 13:17:50 -0700

    route to retrieve stored added

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
+const notes = require('./db/db.json')
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html'))
 );
@@ -12,6 +15,7 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html'
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
+app.get('/api/notes', (req, res) => res.json(notes));
 
 //Question for self: does this part need to be changed for Heroku?
 app.listen(PORT, () =>
