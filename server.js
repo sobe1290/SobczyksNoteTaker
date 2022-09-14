7698ef0aac450f61303ccd3d679434c0d39305cf
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-09-13 17:39:55 -0700

    adding route for post to add note to left

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-const notes = require('./db/db.json')
+const notes = require('./db/db.json');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3001;
@@ -16,6 +17,26 @@ app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.
 );
 
 app.get('/api/notes', (req, res) => res.json(notes));
+
+app.post('/api/notes', (req, res) => {
+  const { title, text } = req.body;
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+    };
+
+    fs.readFile('./db/db.json', function (err, data) {
+      var parsedobj = JSON.parse(data);
+      parsedobj.push(newNote)
+      fs.writeFile('./db/db.json', JSON.stringify(parsedobj), (err) =>
+      err
+      ? console.error(err)
+      : console.log(`New Note --${newNote.title}-- added`)
+      );  
+    })  
+    };
+  });
 
 //Question for self: does this part need to be changed for Heroku?
 app.listen(PORT, () =>
